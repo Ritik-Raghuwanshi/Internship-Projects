6301e4257d3d07b174f65b9c5b8076ee0d7b9d74
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-15 19:42:58 +0530

    working applications data rpvide routes fetch url results using postman backend functionalities done

--- Job-Listing-Portal/backend/controllers/application.controller.js
+++ Job-Listing-Portal/backend/controllers/application.controller.js
@@ -14,7 +14,7 @@ export const applyJob = async (req, res) => {
 
         //check user is already applied for the job
         const existingApplication = await Application.findOne({job: jobId, applicant: userId});
-        if (!existingApplication) {
+        if (existingApplication) {
             return res.status(400).json({
                 message:"You have already applied for this job.",
                 success:false
@@ -22,7 +22,7 @@ export const applyJob = async (req, res) => {
         };
 
         //check if the job exist?
-        const job = await Job.find(jobId);
+        const job = await Job.findById(jobId);
         if (!job) {
             return res.status(404).json({
                 message:"Job not found.",
