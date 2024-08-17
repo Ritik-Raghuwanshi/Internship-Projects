60a871e5b86d5d7db36efddd7895c518f652acd9
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-17 21:37:45 +0530

    provide routing to application via app file give proper paths to access particular web page

--- Job-Listing-Portal/frontend/src/App.jsx
+++ Job-Listing-Portal/frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css'
-import Navbar from './components/shared/Navbar';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Home from './components/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home />
+  },
+  {
+    path:'/login',
+    element:<Login />
+  },
+  {
+    path:'/signup',
+    element:<Signup />
+  }
+])
 
 function App() {
 
   return (
     <>
-    <Navbar />
+    <RouterProvider router={appRouter} />
     </>
   )
 }
