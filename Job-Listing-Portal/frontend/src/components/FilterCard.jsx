6bb44d2bc2cb4129c8f268b3b9dc2e481bb19d9f
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-19 01:00:01 +0530

    buid filter card to show job details

--- Job-Listing-Portal/frontend/src/components/FilterCard.jsx
+++ Job-Listing-Portal/frontend/src/components/FilterCard.jsx
@@ -1,8 +1,45 @@
+import { Label } from "./ui/label"
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
+
+const filterData = [
+  {
+    filterType:"Location",
+    array:["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+  },
+  {
+    filterType:"Industry",
+    array:["Frontend Developer", "Backend Devleloper", "FullStack Developer"]
+  },
+  {
+    filterType:"Salary",
+    array:["0-40k", "42k-1lakh", "1lakh-to-5lakh"]
+  },
+]
 
 const FilterCard = () => {
   return (
-    <div>
-      Filter Card
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter Jobs</h1>
+      <hr className="mt-3" />
+      <RadioGroup>
+        {
+          filterData.map((data, index) => (
+            <div key={index}>
+              <h1 className="font-semibold text-lg my-3" >{data.filterType}</h1>
+              {
+                data.array.map((item, index) => {
+                  return (
+                    <div key={index} className="flex items-center space-x-2 my-2">
+                      <RadioGroupItem value={item} />
+                      <Label>{item}</Label>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          ))
+        }
+      </RadioGroup>
     </div>
   )
 }
