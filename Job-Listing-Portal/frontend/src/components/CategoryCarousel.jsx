db51aa7cffec12bb1b1968ca58561cc5fcba64e4
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-18 16:16:13 +0530

    developement categories carousel

---
+++ Job-Listing-Portal/frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,32 @@
+import { Button } from "./ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+
+const category = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer"
+]
+
+const CategoryCarousel = () => {
+  return (
+    <div className="w-full max-w-xl mx-auto my-20">
+      <Carousel>
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <Button variant="outline" className="rounded-full">
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
+
+export default CategoryCarousel;
