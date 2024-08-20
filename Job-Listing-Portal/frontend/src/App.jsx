341eed0bb8f244de4083b44dd68be24b7d68a291
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-20 19:40:23 +0530

    admin navigate to specific pages and add in  constant component  register company path url

--- Job-Listing-Portal/frontend/src/App.jsx
+++ Job-Listing-Portal/frontend/src/App.jsx
@@ -6,6 +6,9 @@ import Jobs from './components/Jobs';
 import Browse from './components/Browse';
 import Profile from './components/Profile';
 import JobDescription from './components/JobDescription';
+import Companies from './components/admin/Companies';
+import CompanyCreate from './components/admin/CompanyCreate';
+import CompanySetup from './components/admin/CompanySetup';
 
 const appRouter = createBrowserRouter([
   {
@@ -35,7 +38,21 @@ const appRouter = createBrowserRouter([
   {
     path:'/profile',
     element:<Profile />
-  }
+  },
+
+  // Admin paths
+  {
+    path:'/admin/companies',
+    element:<Companies />
+  },
+  {
+    path:'/admin/companies/create',
+    element:<CompanyCreate />
+  },
+  {
+    path:'/admin/companies/:id',
+    element:<CompanySetup />
+  },
 ])
 
 function App() {
