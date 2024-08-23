7f93f8da79a7a0f953e3cd6c38d0b4797e467fd6
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-23 09:46:21 +0530

    display user applied jobs to browse

--- Job-Listing-Portal/frontend/src/components/Browse.jsx
+++ Job-Listing-Portal/frontend/src/components/Browse.jsx
@@ -1,19 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
 import JobCard from "./JobCard";
 import Navbar from "./shared/Navbar"
+import useGetAllJobs from "@/hooks/useGetAllJobs";
+import { useEffect } from "react";
+import { setSearchedQuery } from "./redux/jobSlice";
 
-const randomJobs = [1, 2, 3];
+// const randomJobs = [1, 2, 3];
 
 const Browse = () => {
+  useGetAllJobs();
+  const {allJobs} = useSelector(store=>store.job);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    return () => {
+      dispatch(setSearchedQuery(""));
+    }
+  })
   return (
     <div>
       <Navbar />
       <div className="max-w-7xl mx-20 my-10">
-        <h1 className="font-semibold text-lg my-10">Search Results ({randomJobs.length})</h1>
+        <h1 className="font-bold text-lg my-10">Search Results ({allJobs.length})</h1>
         <div className="grid grid-cols-3 gap-4 mt-5">
         {
-            randomJobs.map((item, index) => {
+            allJobs.map((job) => {
                 return (
-                    <JobCard key={index} />
+                    <JobCard key={job._id} job={job} />
                 )
             })
         }
