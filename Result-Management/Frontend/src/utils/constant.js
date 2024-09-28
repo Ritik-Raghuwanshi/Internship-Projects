3b1d959b4c0bb986ecfd35173bfcc17077cad3c7
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-28 16:49:15 +0530

    Update constant.js

--- Result-Management/Frontend/src/utils/constant.js
+++ Result-Management/Frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const ADMIN_API_END_POINT = "http://internship-projects-six.vercel.app/api/v1/admin";
-export const RESULTS_API_END_POINT = "http://internship-projects-six.vercel.app/api/v1/result";
-export const DASHBOARD_API_END_POINT = "http://internship-projects-six.vercel.app/api/v1/dashboard";
-export const UPLOAD_API_END_POINT = "http://internship-projects-six.vercel.app/api/v1/excelupload";
+export const ADMIN_API_END_POINT = "http://localhost:3000/api/v1/admin";
+export const RESULTS_API_END_POINT = "http://localhost:3000/api/v1/result";
+export const DASHBOARD_API_END_POINT = "http://localhost:3000/api/v1/dashboard";
+export const UPLOAD_API_END_POINT = "http://localhost:3000/api/v1/excelupload";
